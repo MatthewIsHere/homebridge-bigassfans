b68fd638294ce9809878cd7127af1dd5c7dcae9b
Matthew Lyon <matthew@lyon.systems>
2024-03-06 01:50:05 -0500

    config fixes

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = "BigAssPlatform"
+export const PLATFORM_NAME = "BigAssFans"
 
 /**
  * This must match the name of your plugin as defined the package.json
